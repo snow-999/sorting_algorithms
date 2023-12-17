c9a04bd95484d4400e468afba471bc1437cce7f1
khaled <khaledleader012@gmail.com>
2023-12-17 11:58:16 -0800

    m

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * selection_sort - selcetion sort algorethim
+ * @array: an array will be sorted
+ * @size: size of the array
+ * Return: void
+ *
+*/
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, k;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = size - 1, k = i + 1; j > i; j--)
+		{
+			if (array[k] > array[j])
+			{
+				k = j;
+			}
+		}
+		if (array[i] > array[k])
+		{
+			temp = array[i];
+			array[i] = array[k];
+			array[k] = temp;
+			print_array(array, size);
+		}
+	}
+}
