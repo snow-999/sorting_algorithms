57912cb86a194d36b331800d98bee5546a284b05
khaled <khaledleader012@gmail.com>
2023-12-17 09:14:47 -0800

    m

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorting algerothim
+ * @array: an array will be sorted
+ * @size: the size of an array
+ * Return: void
+ *
+*/
+void bubble_sort(int *array, size_t size)
+{
+	int tmp;
+	int j, i = 0;
+
+	while (i < size)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] < array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+readme file for algo and big o task
